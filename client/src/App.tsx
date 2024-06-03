888d84ca9fa938a8900ebec6f83807f812fc1235
Aditya Choudhury <adityasubham03@gmail.com>
2024-06-03 08:23:59 +0530

    Added tailwind support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <div className='text-2xl text-indigo-500'>Hello world</div>
+    </>
   )
 }
 
